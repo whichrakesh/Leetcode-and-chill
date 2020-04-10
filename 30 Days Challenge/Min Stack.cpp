ab048eaced0dcf513d17c867c35eeea210c53c67
Rakesh Ranjan Nayak <whichrakesh@gmail.com>
2020-04-11 01:12:14 +0530

    Changes

---
+++ 30 Days Challenge/Min Stack.cpp
@@ -0,0 +1,42 @@
+class MinStack {
+    stack<int> s1, s2;
+public:
+    
+    /** initialize your data structure here. */
+    MinStack() {
+        s1 = stack<int>();
+        s2 = stack<int>();
+    }
+    
+    void push(int x) {
+        s1.push(x);
+        if(s2.empty() || x <= getMin()){
+            s2.push(x);
+        }
+    }
+    
+    void pop() {
+        int x = s1.top();
+        s1.pop();
+        if(!s2.empty() && x == getMin()){
+            s2.pop();
+        }
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
